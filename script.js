f396001f3b60ab838fc06771b5d50913ae06f22b
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-12 21:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function firstChar(text) {
 	if(text==="") return "";
 	let count=0;
 	for(let i=0;i<text.length;i++){
-		if((text[i]>='a' && text[i]<='z') || (text[i]>='A' && text[i]<='Z') return text[i] ))
+		if((text[i]>='a' && text[i]<='z') || (text[i]>='A' && text[i]<='Z')) return text[i]; 
          count++;
 	}
 	if(count==text.length) return "";
