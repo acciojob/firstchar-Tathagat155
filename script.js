ec89f21ba958118ffc1edefa2a8e5bba681290de
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-12 22:46:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 function firstChar(text) {
   // your code here
-	
-	if(text==="") return "";
-	let count=0;
 	for(let i=0;i<text.length;i++){
 		if((text[i]>='a' && text[i]<='z') || (text[i]>='A' && text[i]<='Z')) return text[i]; 
-         count++;
 	}
-	if(count==text.length) return "";
+	return "";  
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
